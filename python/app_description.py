ba6f2566a3101a132392aefd119238bb377514d1
48323 - Simão Cabral <a48323@alunos.isel.pt>
2025-02-01 12:26:58 +0000

    app description completed

--- python/app_description.py
+++ python/app_description.py
@@ -1,3 +1,8 @@
-
+import requests
 
 search_link = "https://ws75.aptoide.com/api/7/app/get/store_name=apps/q=bWF4U2RrPTE5Jm1heFNjcmVlbj1ub3JtYWwmbWF4R2xlcz0yLjA/package_name=com.fun.lastwar.gp/language=pt_PT/"
+
+response = requests.get(search_link)
+body = response.json()
+description = body['nodes']['meta']['data']['media']['description']
+print(description)
